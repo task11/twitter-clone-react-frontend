0480f023c9be48f729305fc947804a77ce0fb8f1
task11 <task11@naver.com>
2021-11-23 01:14:41 +0900

    Routing components

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,8 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom"
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
-import Profile from "../routes/Profile";
-import EditProfie from "../routes/EditProfie";
+import AppRouter from "./Router";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
-        <Route
-          path="/auth"
-          element={<Auth />}
-        />
-        <Route
-          path="/profile"
-          element={<Profile />}
-        />
-        <Route
-          path="/editprofile"
-          element={<EditProfie />}
-        />
-      </Routes>
-    </Router>
+    <AppRouter />
   );
 }
 
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
 
-export default () => {
+const AppRouter = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <Router>
-      <Switch>
-
-      </Switch>
+      <Routes>
+        {isLoggedIn ?
+          <Route
+            path="/"
+            element={<Home />}
+          /> :
+          <Route
+            path="/"
+            element={<Auth />}
+          />
+        }
+      </Routes>
     </Router>
   )
 }
+
+export default AppRouter;
