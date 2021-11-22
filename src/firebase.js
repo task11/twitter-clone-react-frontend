69fb48ea7c608b7c14332f714d22ece2e41705f9
task11 <task11@naver.com>
2021-11-22 13:48:52 +0900

    Install & Setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB35N1IWdPRuKxsQmeVMjILPhl-CNsD76A",
+  authDomain: "twitterclone-f01bf.firebaseapp.com",
+  projectId: "twitterclone-f01bf",
+  storageBucket: "twitterclone-f01bf.appspot.com",
+  messagingSenderId: "784145596468",
+  appId: "1:784145596468:web:33be123db378b798240a61"
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
+
