c61a1dbe4ca3a2f494a7ed5da1b1332f43509d2c
task11 <task11@naver.com>
2021-11-25 23:14:48 +0900

    Add firestore Database

--- src/myBase.js
+++ src/myBase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import * as firebase from "firebase/app";
-
+import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -14,3 +13,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const authService = getAuth();
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,35 @@
-import React from "react";
+import { dbService } from "myBase";
+import React, { useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const add = await addDoc(collection(dbService, "tweets"), {
+      tweet,
+      createdAt: Date.now(),
+
+    });
+    setTweet("");
+  };
+  const onChange = (event) => {
+    const { target: { value } } = event;
+    setTweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          type="text"
+          onChange={onChange}
+          placeholder="What's on your mind?"
+          maxLength="{120}"
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
