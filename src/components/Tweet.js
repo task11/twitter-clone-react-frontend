60a82ddf8ff260344fe621aa341da54538bebd08
task11 <task11@naver.com>
2021-11-28 20:28:47 +0900

    Add toggle flag

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,8 +1,10 @@
 import { deleteDoc, doc } from "@firebase/firestore";
 import { dbService } from "myBase";
-import React from "react";
+import React, { useState } from "react";
 
 const Tweet = ({ tweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newTweet, setNewTweet] = useState(tweetObj.text);
   const tweetTextRef = doc(dbService, "tweets", `${tweetObj.id}`);
   const onDeleteClick = async () => {
     const ok = window.confirm("Are you sure you want to delete this tweet?");
@@ -10,18 +12,37 @@ const Tweet = ({ tweetObj, isOwner }) => {
     if (ok) {
       await deleteDoc(tweetTextRef);
     }
-  }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev); // 토글 상태값 변경
 
   return (
     <div>
-      <h4>{tweetObj.text}</h4>
-      {isOwner && (
-        <>
-          <button onClick={onDeleteClick} >Delete Tweet</button>
-          <button>Edit Tweet</button>
-        </>
-      )}
-
+      {
+        editing
+          ? (
+            <>
+              <form>
+                <input value={newTweet} required />
+              </form>
+              <button onClick={toggleEditing}>Cancel</button>
+            </>
+          ) : (
+            <>
+              <h4>
+                {tweetObj.text}
+              </h4>
+              {
+                isOwner &&
+                (
+                  <>
+                    <button onClick={onDeleteClick} >Delete Tweet</button>
+                    <button>Edit Tweet</button>
+                  </>
+                )
+              }
+            </>)
+      }
     </div>
   )
 }
