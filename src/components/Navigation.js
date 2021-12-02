096b1494f396014492ed22d9f812fcfb9bc385c0
task11 <task11@naver.com>
2021-12-02 20:39:48 +0900

    Add user display name in Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navigation = () => <nav>
-  <ul>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/profile">My Profile</Link>
-    </li>
-  </ul>
-</nav >;
+const Navigation = ({ userObj }) => {
+  return <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/profile">{userObj.displayName}의 Profile</Link>
+      </li>
+    </ul>
+  </nav >
+};
 
 export default Navigation;
